4b8c0404fea1c09d498d1360f4dd34603030ae32
estrela801 <rohenri0808@gmail.com>
2024-08-08 17:46:05 -0300

    feat: tentando adicionar animação para o mobile

--- react-app/src/components/Links/Links.jsx
+++ react-app/src/components/Links/Links.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import './Link.css'
+import './MobileAnimations.css'
 
 export default (props) => {
 const In = props.in
 const insta = props.insta
 const git = props.git
 
+function teste(){
+  const testeAnimation = document.querySelector('.teste').style.display='none'
+  const links = document.querySelectorAll('Link')
+  links.forEach(link =>{
+    link.classList.add('isboucing')
+  
+  })
+  if(!testeAnimation){
+   
+  }
+}
+document.addEventListener('DOMContentLoaded', teste)
+
+
   return (
     <div className='container-links'>
       <a href="https://www.instagram.com/estrela_dev801/" target="_blank" className="Link">Instagram <img className="icon" src={insta} alt="" /> </a>
       <a href="https://github.com/estrela801" target="_blank" className="Link">Git Hub<img className="icon" src={git} alt="" /></a>
       <a href="https://www.linkedin.com/in/rodrigohestrela/" target="_blank" className="Link">LinkedIn <img className="icon" src={In} alt="" /></a>
+      <span className="teste"></span>
     </div>
   );
 };
