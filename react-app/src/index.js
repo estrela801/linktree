7682c0c9b25323f34fcd3ec0128be000e317c453
estrela801 <rohenri0808@gmail.com>
2024-08-08 15:08:18 -0300

    feat: adicionando reset para melhor eficiência do projeto

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import './reset.css'
 import './index.css'
 
 // Renderiza o componente App dentro da div com ID 'root'
