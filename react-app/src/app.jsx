9ee92eb35d8561ddd9ea6cb4eb2856d07ffb7f01
estrela801 <rohenri0808@gmail.com>
2024-08-08 15:23:15 -0300

    feat: adicionando novos componentes

--- react-app/src/app.jsx
+++ react-app/src/app.jsx
@@ -1,10 +1,16 @@
-import './app.css'
 import React from "react";
+import Container from "./components/Container/Container";
+import Links from "./components/Links/Links";
+import Imagens from "./components/Imgs/Imagens";
+import Img from './assets/eu.jpg'
 
 export default (props) => {
   return (
     <div>
-      <h1>ahkdhsauhd</h1>
+      <Container>
+            <Imagens img={Img}></Imagens>
+            <Links link=''></Links>
+      </Container>
     </div>
   );
 };
