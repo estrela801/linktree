93095599cb2bab69ab2e7035ebf4872522565622
estrela801 <rohenri0808@gmail.com>
2024-08-08 15:24:29 -0300

    feat: melhorando o componente de links

---
+++ react-app/src/components/Links/Links.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default (props) => {
+  return (
+    <div className='container-links'>
+      <a href="" className="Link">Instagram</a>
+      <a href="" className="Link">Git Hub</a>
+      <a href="" className="Link">LinkedIn</a>
+    </div>
+  );
+};
