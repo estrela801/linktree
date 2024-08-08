f93f7727992683de99d6cc93f191aa6d4ac6385d
estrela801 <rohenri0808@gmail.com>
2024-08-08 17:01:41 -0300

    feat: adicionando icons ao projeto e estilizando o fundo

--- react-app/src/app.jsx
+++ react-app/src/app.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import './app.css'
+import React from "react";  
 import Container from "./components/Container/Container";
 import Links from "./components/Links/Links";
 import Imagens from "./components/Imgs/Imagens";
 import Img from './assets/eu.jpg'
+import Insta from './assets/icons8-insta-30.png'
+import In from './assets/icons8-linkedin-30.png'
+import Git from './assets/icons8-github-30.png'
+
 
 export default (props) => {
   return (
-    <div>
+    <div className='container-geral'>
       <Container>
             <Imagens img={Img}></Imagens>
-            <Links link=''></Links>
+            <Links link='' insta={Insta} git={Git} in={In} ></Links>
       </Container>
     </div>
   );
