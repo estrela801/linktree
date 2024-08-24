705c1e0ef8d24f7462852f4975de0f454bacdf82
estrela801 <rohenri0808@gmail.com>
2024-08-24 20:44:47 -0300

    feat: adicionando link para contato no whatsapp

--- react-app/src/components/Links/Links.jsx
+++ react-app/src/components/Links/Links.jsx
@@ -20,13 +20,14 @@ function teste(){
 }
 document.addEventListener('DOMContentLoaded', teste)
 
-
+const whats = <span ><a className="whats Link icon" href="https://wa.me/5511918721662?text=Olá,%20vim%20pelo%20seu%20linktree" target="_blank">Enviar mensagem</a></span>
   return (
     <div className='container-links'>
       <a href="https://www.instagram.com/estrela_dev801/" target="_blank" className="Link">Instagram <img className="icon" src={insta} alt="" /> </a>
       <a href="https://github.com/estrela801" target="_blank" className="Link">Git Hub<img className="icon" src={git} alt="" /></a>
       <a href="https://www.linkedin.com/in/rodrigohestrela/" target="_blank" className="Link">LinkedIn <img className="icon" src={In} alt="" /></a>
       <span className="teste"></span>
+      {whats}
     </div>
   );
 };
